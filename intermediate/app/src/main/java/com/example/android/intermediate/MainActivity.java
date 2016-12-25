9e94989392f74241e1a1db6f67ce2bdc3c8c75f0
ShehryaAhmed <shehreyarahmed5@gmail.com>
2016-12-25 20:16:28 +0500

    set layout and create instant edit and text views..

--- intermediate/app/src/main/java/com/example/android/intermediate/MainActivity.java
+++ intermediate/app/src/main/java/com/example/android/intermediate/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.android.intermediate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText mSearchBoxEditText;
+
+    TextView mURLDisplayTextview;
+
+    TextView mSearchResult;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSearchBoxEditText = (EditText) findViewById(R.id.ed_search_box);
+
+        mURLDisplayTextview = (TextView)findViewById(R.id.tv_display_url);
+
+        mSearchResult   = (TextView) findViewById(R.id.tv_github_search_result);
+
+
     }
+
+
+    
 }
--- intermediate/app/src/main/java/com/example/android/intermediate/utilities/Networkutilities.java
+++ intermediate/app/src/main/java/com/example/android/intermediate/utilities/Networkutilities.java
@@ -1,7 +1,39 @@
 package com.example.android.intermediate.utilities;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.util.Scanner;
+
 /**
  * Created by android on 12/25/2016.
  */
 public class Networkutilities {
+    final static String GITHUB_BASE_URL = "https://api.github.com/search/repositories";
+    final static String PARAM_QUERY = "q";
+    final static String PARAM_SORT = "sort";
+    final static String sortBy = "stars";
+
+    public static URL buildUrl(String githubSearchQuery) {
+        return null;
+    }
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
 }
